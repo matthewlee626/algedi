adba731fcb284e7efe5ca4ef6aea656ea9dff98f
Matthew Lee <matthewlee626@gmail.com>
2020-05-23 19:30:40 -0700

    updates

---
+++ client/build/precache-manifest.5e65da460ac7bb602cc1be8993862079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93e61e7854824ddf37e7186480cdce2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20ca6037d1c805ea13e8",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9539246923f875d09a6e",
+    "url": "/static/js/2.ef96b227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef96b227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ca6037d1c805ea13e8",
+    "url": "/static/js/main.1246201f.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
